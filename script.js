6212712d0877c8f99b5597b67f67d2235d525db6
Everton2312 pereira <vilmamariadasilvaaa@gmail.com>
2024-04-22 00:04:24 +0000

    javascript do agradcimento

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let botoes = document.querySelectorAll(".botoes button")
-.botoes.forEach(botao =>{
-    botao.addEventlistener("click", guardarNota)
+botoes.forEach(botao => {
+    botao.addEventListener("click", guardarNota)
 })
 
 function guardarNota(evento) {
-       let nota = evento.target.innerText
-       
-       localStorage.setItem("nota", nota)
+    let nota = evento.target.innerText
+
+localStorage.setItem("nota", nota)
 }
